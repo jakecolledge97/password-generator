ae1f492e09b8eafe432ed382bb0cc107cb35349f
John Conway <88188872+jakecolledge97@users.noreply.github.com>
2021-09-17 12:05:50 +0930

    first commit

---
+++ Develop/script.js
@@ -0,0 +1,64 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+  
+  
+  //Function to create password
+  function generatePassword(){
+
+    //lets user choose how long the password can be
+    var passwordLength = prompt("How long would you like your password to be, between 8 and 128?")
+    
+    //checks if passwordLength is between 8-128
+    function checkPasswordLength(){
+      if(passwordLength < 8 || passwordLength > 128){
+        passwordLength = prompt("Make sure the number you choose is between 8 and 128.")
+        checkPasswordLength()
+      }
+    }
+    checkPasswordLength()
+
+    //Characters that password can use to be created
+    var passwordOptions = {
+      //base alphabet
+      alphabet: "abcdefghijklmnopqrstuvwxyz",
+      //with capitals
+      alphabetCapital: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+      //with numbers
+      alphabetNumbers: "0123456789",
+      //with symbols
+      alphabetSymbols: "\"\\ !#$%&'()*+,-./:;<=>?@[]^_`{|}~"      
+    }
+    var selection = ""
+    //password parameters
+    if(confirm("Do you want you password to contain letters?")){
+      selection += passwordOptions.alphabet
+    }
+    if(confirm("Do you want your password to contain capital letters?")){
+      selection += passwordOptions.alphabetCapital
+    }
+    if(confirm("Do you want your password to contain numbers?")){
+      selection += passwordOptions.alphabetNumbers
+    }
+    if(confirm("Do you want your password to contain symbols?")){
+      selection += passwordOptions.alphabetSymbols
+    }
+    var finalPassword = ""
+    //Loop that adds letters to password
+    for(i=1; i<=passwordLength;i++){
+      finalPassword += selection.charAt(Math.floor(Math.random()*selection.length))
+    }
+    
+    
+    return finalPassword;
+  }
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
